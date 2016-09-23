a9c7a4c571221e2a4d023f19fd6a485892fceb62
eRaz0rHead <eRaz0rHead@gmail.com>
2016-09-22 18:24:31 -0700

    Update running-lights.cs

--- running-lights.cs
+++ running-lights.cs
@@ -1,15 +1,26 @@
 Vector3D column;
 Vector3D[][] lightTable;
 
+var startLight = null ; // getBlockByName... assert in Group.
+
 double min_dist = 0;
-var first = lights[0].GetPosition();
-for (int i = 10; i < lights.length; i++) {
+for (int i = 0; i < lights.length; i++) {
+	if (startLight == lights[i]) continue;
 	var currentPosition = lights[i].GetPosition();
-	var d = Vector3D.Distance(first, currentPosition);
+	var d = Vector3D.Distance(startLight, currentPosition);
 	if (d < min_dist || column == null) {
 		min_dist = d;
 		column = currentPosition - first;
 	}
 }
 
+for (int i = 0; i < lights.length; i++) {
+	if (startLight == lights[i]) continue;
+	var currentPosition = lights[i].GetPosition();
+	var d = Vector3D.Distance(startLight, currentPosition);
+	if (d < min_dist || column == null) {
+		min_dist = d;
+		column = currentPosition - first;
+	}
+}
 	
