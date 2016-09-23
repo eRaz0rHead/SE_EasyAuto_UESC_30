9f9f130a23fcc6a0188edf7b636198b80479ef20
eRaz0rHead <eRaz0rHead@gmail.com>
2016-09-22 18:10:43 -0700

    Update running-lights.cs

--- running-lights.cs
+++ running-lights.cs
@@ -1,17 +1,15 @@
-Vector column;
+Vector3D column;
 Vector3D[][] lightTable;
 
 double min_dist = 0;
-var door = ag.doors[0];
 var first = lights[0].GetPosition();
 for (int i = 10; i < lights.length; i++) {
-	var d = Vector3D.Distance(first, lights[i].GetPosition());
+	var currentPosition = lights[i].GetPosition();
+	var d = Vector3D.Distance(first, currentPosition);
 	if (d < min_dist || column == null) {
 		min_dist = d;
-		column = i;
+		column = currentPosition - first;
 	}
 }
-for (int i=1; i < lights.length; i++) {
-  	if (Vector3D.Distance(light[i].GetPosition(), lights[0].GetPosition())
-}
+
 	
